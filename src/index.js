d524eac841b555803a0cd4c92e9ed00fcf47b607
Andrey Ponomarev <info@andreyponomarev.ru>
2019-11-22 12:04:00 +0300

    Add header, footer

--- src/index.js
+++ src/index.js
@@ -8,30 +8,27 @@ import './blocks/_reset.scss';
  **********/
 
 /***************** Redefinition level 'blocks.biscuit-lib' *******************/
-/*
-import './src/blocks/text/_text.scss';
 
-import './src/blocks/heading/_heading.scss';
+import './blocks/text/_text.scss';
 
-import './src/blocks/btn/_btn.scss';
-import './src/blocks/btn/btn.js';
+import './blocks/heading/_heading.scss';
 
-import './src/blocks/grid-forms/_grid-forms.scss';
+import './blocks/btn/_btn.scss';
 
-import './src/blocks/hr/_hr.scss';
+import './blocks/grid-forms/_grid-forms.scss';
 
-import './src/blocks/control/_control.scss';
+import './blocks/control/_control.scss';
 
-import './src/blocks/form-group/_form-group.scss';
+import './blocks/form-group/_form-group.scss';
 
-import './src/blocks/nav/_nav.scss';
-import './src/blocks/nav/nav.js';
+import './blocks/header/_header.scss';
 
-import './src/blocks/select/_select.scss';
-*/
+import './blocks/select/_select.scss';
+
+import './blocks/embed/_embed.scss';
+
 /***************** Redefinition level 'blocks' *******************/
 
-/*
 import './blocks/page/_page.scss';
 import './blocks/page/page.js';
 
@@ -41,7 +38,8 @@ import './blocks/grid-page-layout2/_grid-page-layout2.scss';
 
 import './blocks/main/_main.scss';
 
-import './blocks/header/_header.scss';
-
 import './blocks/footer/_footer.scss';
-*/
+
+import './blocks/logo/_logo.scss';
+
+import './blocks/section-otzivi/_section-otzivi.scss';
