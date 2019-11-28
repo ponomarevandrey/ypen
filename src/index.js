3509f7c03cfec29300b938e40dd6d2bf061576b9
Andrey Ponomarev <info@andreyponomarev.ru>
2019-11-28 13:36:27 +0300

    Add new fixes (refactor css, config for modal component as static prop)

--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@ import './blocks/_reset.scss';
  * BLOCKS *
  **********/
 
-/***************** Parent blocks *******************/
-
 import './blocks/text/_text.scss';
 
 import './blocks/heading/_heading.scss';
@@ -21,48 +19,48 @@ import './blocks/control/_control.scss';
 
 import './blocks/form-group/_form-group.scss';
 
-import './blocks/header/_header.scss';
-
 import './blocks/select/_select.scss';
 
 import './blocks/embed/_embed.scss';
 
-/***************** Child blocks *******************/
-
 import './blocks/page/_page.scss';
 
-import './blocks/grid-page-layout1/_grid-page-layout1.scss';
+//import './blocks/grid-page-layout1/_grid-page-layout1.scss';
 
-import './blocks/grid-page-layout2/_grid-page-layout2.scss';
+//import './blocks/grid-page-layout2/_grid-page-layout2.scss';
 
 import './blocks/icons/_icons.scss';
 
-import './blocks/footer/_footer.scss';
-
 import './blocks/logo/_logo.scss';
 
-import './blocks/section9/_section9.scss';
+import './blocks/header/_header.scss';
+
+import './blocks/main/_main.scss';
+
+import './blocks/benefits1/_benefits1.scss';
+
+import './blocks/benefits2/_benefits2.scss';
+
+import './blocks/benefits3/_benefits3.scss';
+
+import './blocks/benefits4/_benefits4.scss';
+
+import './blocks/benefits5/_benefits5.scss';
+
+import './blocks/certificates/_certificates.scss';
+
+import './blocks/webinars/_webinars.scss';
 
 import './blocks/section8/_section8.scss';
 
-import './blocks/section7/_section7.scss';
+import './blocks/reviews/_reviews.scss';
+// import './blocks/reviews/reviews.js';
 
-import './blocks/section6/_section6.scss';
-
-import './blocks/section5/_section5.scss';
-
-import './blocks/section4/_section4.scss';
-
-import './blocks/section3/_section3.scss';
-
-import './blocks/section2/_section2.scss';
-
-import './blocks/section1/_section1.scss';
+import './blocks/footer/_footer.scss';
 
 import './blocks/modal/_modal.scss';
-import './blocks/modal/modal.js';
-import { config as modalConfig, Modal } from './blocks/modal/modal.js';
-new Modal(modalConfig);
+import { Modal } from './blocks/modal/modal.js';
+new Modal();
 
 import './blocks/page/_page.scss';
 import './blocks/page/page.js';
